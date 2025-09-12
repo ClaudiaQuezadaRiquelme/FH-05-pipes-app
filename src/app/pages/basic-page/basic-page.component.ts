9ed7748712913425d702b93fd70bbacaa6204aac
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-12 13:52:55 -0300

    numbers pipes, datePipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,11 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
-    LowerCasePipe, UpperCasePipe, TitleCasePipe
+    LowerCasePipe, UpperCasePipe, TitleCasePipe,
+    DatePipe,
   ],
   templateUrl: './basic-page.component.html',
 })
@@ -12,4 +13,6 @@ export default class BasicPageComponent {
   nameLower = signal('zundamon');
   nameUpper = signal('ZUNDAMON');
   fullName  = signal('MoCHi mOcHI zuNdA');
+
+  customDate = signal(new Date());
 }
--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [
+    DecimalPipe, PercentPipe, CurrencyPipe
+  ],
   templateUrl: './numbers-page.component.html',
 })
 export default class NumbersPageComponent {
-
+  totalSells = signal(23_456_543.8567);
+  percent = signal(0.4856);
 }
