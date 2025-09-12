c22f14a22a71523f91050a23df492209e210e655
cquezada-cygnus <ca.quezada.riquelme@gmail.com>
2025-09-12 17:18:11 -0300

    i18PluralPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'José',
@@ -18,7 +18,7 @@ const client2 = {
 @Component({
   selector: 'app-uncommon-page',
   imports: [
-    CardComponent, I18nSelectPipe
+    CardComponent, I18nSelectPipe, I18nPluralPipe
   ],
   templateUrl: './uncommon-page.component.html',
 })
@@ -37,5 +37,28 @@ export default class UncommonPageComponent {
       return;
     }
     this.client.set(client1);
+  }
+
+
+  // i18n Plural
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemosun par de clientes esperando',
+    other: 'tenemos # clientes esperando',
+  })
+
+  clients = signal([
+    "Butterfree",
+    "Beedrill",
+    "Pidgeot",
+    "Rattata",
+    "Ivysaur",
+    "Charmander",
+    "Squirtle",
+  ]);
+
+  deleteBeing() {
+    this.clients.update( prev => prev.slice(1));
   }
 }
